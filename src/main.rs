6dfc3ed9b13fec7853653a87f1b5f124664c9379
Michael Jarvis <michael.jarvis@wegotpop.com>
2022-09-26 20:54:30 +0100

    refactor for different methods

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,9 @@ use std::{
     thread,
     time::Duration,
 };
+use std::process::Command;
 mod thread_pool;
+
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -24,18 +26,41 @@ fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        thread::sleep(Duration::from_secs(5));        
-        ("HTTP/1.1 200 OK", "hello.html")
+    let request_parts = request_line.split_whitespace();
+
+    let request_vec = request_parts.collect::<Vec<&str>>();
+    let (method, path, _) = (request_vec[0], request_vec[1], request_vec[2]);
+
+    let response: (&str, &str);
+    if method == "GET" {
+        response = get_request(path);
+    } else if method == "POST" {
+        response = post_request(path);
     } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
-    };
+        response = unknown_request();
+    }
+
+    let http = response.0;
+    let filename = response.1;
 
     let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
 
     let response =
-        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+        format!("{http}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
     stream.write_all(response.as_bytes()).unwrap();
+}
+
+fn get_request(path: &str) -> (&str, &str) {
+    if path != "/" {
+        return unknown_request();
+    }
+    ("HTTP/1.1 200 OK", "hello.html")
+}
+fn post_request(path: &str) -> (&str, &str) {
+    ("HTTP/1.1 405 METHOD NOT ALLOWED", "404.html")
+}
+fn unknown_request() -> (&'static str, &'static str) {
+    ("HTTP/1.1 404 NOT FOUND", "404.html")
 }
