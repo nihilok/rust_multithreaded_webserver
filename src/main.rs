bae65db991f922a66ca6295a38fbf1a3beee4076
Michael Jarvis <mjfullstack@gmail.com>
2022-09-28 23:32:21 +0100

    refactor response and HTTP parsing

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,30 @@
 mod response;
 use crate::response::Response;
-
+use http_parser::HTTPRequest;
 use std::{
-    io::{prelude::*, BufReader},
+    io::prelude::*,
     net::{TcpListener, TcpStream},
     collections::HashMap,
     str,
 };
 use threadpool::ThreadPool;
 
-type HTTPFile = (&'static str, &'static str);
+type HTTPFile = (String, String);
 type RouteMap = HashMap<&'static str, fn() -> HTTPFile>;
 
 const HTTP_SUCCESS: &str = "HTTP/1.1 200 OK";
 const HTTP_NOT_FOUND: &str = "HTTP/1.1 404 NOT FOUND";
 const ADDR: &str = "0.0.0.0";
 const PORT: u16 = 8080;
+const THREADPOOL_SIZE: usize = 6;
 
 
 fn main() {
+    let pool = ThreadPool::new(THREADPOOL_SIZE);
     let bind_addr = format!("{}:{}", ADDR, PORT);
     let listener = TcpListener::bind(&bind_addr).unwrap();
     println!("Listening for TCP traffic at http://{}", bind_addr);
 
-    let pool = ThreadPool::new(4);
 
     let routes = register_routes();
 
@@ -31,51 +32,47 @@ fn main() {
         let stream = stream.unwrap();
         let r = routes.to_owned();
         pool.execute(move || {
-            handle_connection(stream, &r);
+            handle_connection(&stream, &r);
         });
     }
 }
 
-fn handle_connection(mut stream: TcpStream, routes: &RouteMap) {
-    let buf_reader = BufReader::new(&mut stream);
-    let request_line = buf_reader.lines().next().unwrap().unwrap();
-
-    let request_parts = request_line.split_whitespace();
-
-    let request_vec = request_parts.collect::<Vec<&str>>();
-    let (_method, path, _version) = (request_vec[0], request_vec[1], request_vec[2]);
-
-    let response = get_response_parts(path, routes);
-    match path {
+fn handle_connection(mut stream: &TcpStream, routes: &RouteMap) {
+    let buffer: HTTPRequest = HTTPRequest::new(&stream);
+    let response = {
+        let request =  buffer.request();
+        get_response_parts(&request.path, routes)
+    };
+    match &buffer.request().path[..] {
         "/download" => {
-            stream.write_all(&response.file_response()).unwrap();
+            let _ = stream.write_all(&response.file_response()).unwrap();
         }
         _ => {
-            stream.write_all(response.simple_response().as_bytes()).unwrap();
+            let _ = stream.write_all(&response.simple_response()).unwrap();
         }
     }
 }
 
-fn get_response_parts<'a>(path: &str, routes: &RouteMap) -> Response<'a> {
+fn get_response_parts(path: &str, routes: &RouteMap) -> Response {
     let mut success = true;
     let filename: HTTPFile = match routes.get(path) {
         None => {
             success = false;
-            ("404.html", "text/html")
+            ("404.html".to_string(), "text/html".to_string())
         }
         Some(route) => { route() }
     };
-    let status_line = if success { HTTP_SUCCESS } else { HTTP_NOT_FOUND };
+    let status_line = if success { HTTP_SUCCESS.to_string() } else { HTTP_NOT_FOUND.to_string() };
     let response = Response::new(status_line, filename.0, Some(filename.1));
     return response;
 }
 
 fn index() -> HTTPFile {
-    ("hello.html", "text/html")
+    ("hello.html".to_string(), "text/html".to_string())
 }
 
 fn download() -> HTTPFile {
-    ("test.mp3", "audio/mpeg")
+    ("test.mp3".to_string(), "audio/mpeg".to_string())
 }
 
 fn register_routes() -> RouteMap {
--- src/response.rs
+++ src/response.rs
@@ -1,28 +1,29 @@
 use std::{fs, str};
 
-pub struct Response<'a> {
-    status_line: &'a str,
-    content_type: Option<&'a str>,
+pub struct Response {
+    status_line: String,
+    content_type: Option<String>,
+    filename: String,
     contents: Vec<u8>,
 }
 
-impl<'a> Response<'a> {
-    pub fn new(status_line: &'a str, filename: &'a str, content_type: Option<&'a str>) -> Response<'a> {
-        let contents = fs::read(filename).unwrap();
-        Response { status_line, content_type, contents }
+impl Response {
+    pub fn new(status_line: String, filename: String, content_type: Option<String>) -> Response {
+        let contents = fs::read(&filename).unwrap();
+        Response { status_line, content_type, filename, contents }
     }
     pub fn len(&self) -> usize {
         self.contents.len()
     }
-    pub fn simple_response(&self) -> String {
+    pub fn simple_response(&self) -> Vec<u8> {
         let content = str::from_utf8(&self.contents).unwrap();
         let http =
             format!("{}\r\nContent-Length: {}\r\n\r\n{}", self.status_line, self.len(), content);
-        return http;
+        return Vec::from(http.as_bytes());
     }
     pub fn file_response(&self) -> Vec<u8> {
         let bytes_string = &self.contents[..];
-        let content_type = match self.content_type {
+        let content_type = match &self.content_type {
             None => { "application/octet-stream" }
             Some(c_type) => { c_type }
         };
@@ -32,10 +33,11 @@ impl<'a> Response<'a> {
                 Connection: keep-alive\r\n\
                 Content-Length: {}\r\n\
                 Content-Type: {}\r\n\
-                content-disposition: attachment; filename*=download.mp3\r\n\r\n",
+                content-disposition: attachment; filename*={}\r\n\r\n",
                 self.status_line,
                 self.len(),
                 content_type,
+                self.filename
             );
         let response_bytes = &http.as_bytes();
         let response = [response_bytes, bytes_string].concat();
