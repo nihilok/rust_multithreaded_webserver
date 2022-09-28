fae69fe55ec3debf47f37a3234eb1e14b427fe95
Michael Jarvis <michael.jarvis@wegotpop.com>
2022-09-28 11:33:44 +0100

    tidy up

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,14 @@ use std::{
     fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
-    thread,
-    time::Duration,
     collections::HashMap,
+    str,
 };
-use std::process::Command;
 use threadpool::ThreadPool;
 use format_bytes::format_bytes;
 
 type FileResponse = (&'static str, &'static str);
-type RouteMap = HashMap<&'static str, fn(&str) -> FileResponse>;
+type RouteMap = HashMap<&'static str, fn() -> FileResponse>;
 
 
 fn main() {
@@ -38,32 +36,43 @@ fn handle_connection(mut stream: TcpStream, routes: &RouteMap) {
     let (_method, path, _version) = (request_vec[0], request_vec[1], request_vec[2]);
 
     let response = get_response_parts(path, routes);
-    // stream.write_all(response.simple_response().as_bytes()).unwrap();
+    match path {
+        "/download" => {
+            stream.write_all(response.file_response().as_bytes()).unwrap();
+        }
+        _ => {
+            stream.write_all(response.simple_response().as_bytes()).unwrap();
+        }
+    }
     stream.write_all(response.file_response().as_bytes()).unwrap();
 }
 
 struct Response<'a> {
     status_line: &'a str,
-    content_type: &'a str,
+    content_type: Option<&'a str>,
     contents: Vec<u8>,
-    filename: &'a str,
 }
 
 impl<'a> Response<'a> {
-    fn new(status_line: &'a str, filename: &'a str, content_type: &'a str) -> Response<'a> {
+    fn new(status_line: &'a str, filename: &'a str, content_type: Option<&'a str>) -> Response<'a> {
         let contents = fs::read(filename).unwrap();
-        Response { status_line, content_type, contents, filename }
+        Response { status_line, content_type, contents }
     }
     fn len(&self) -> usize {
         self.contents.len()
     }
     fn simple_response(&self) -> String {
+        let content = str::from_utf8(&self.contents).unwrap();
         let http =
-            format!("{}\r\nContent-Length: {}\r\n\r\n{:?}", self.status_line, self.len(), self.contents);
+            format!("{}\r\nContent-Length: {}\r\n\r\n{}", self.status_line, self.len(), content);
         return http;
     }
     fn file_response(&self) -> String {
         let bytes_string = format_bytes!(b"{}", &self.contents);
+        let content_type = match self.content_type {
+            None => { "application/octet-stream" }
+            Some(c_type) => { c_type }
+        };
         let http =
             format!(
                 "{}\r\n\
@@ -74,7 +83,7 @@ impl<'a> Response<'a> {
                 {:?}",
                 self.status_line,
                 self.len(),
-                self.content_type,
+                content_type,
                 bytes_string
             );
         return http;
@@ -82,30 +91,26 @@ impl<'a> Response<'a> {
 }
 
 fn get_response_parts<'a>(path: &str, routes: &RouteMap) -> Response<'a> {
-    let parts = if routes.contains_key(path) { routes.get(path).unwrap()(path) } else { unknown_request() };
-    let response = Response::new(parts.0, parts.1, "audio/mpeg");
-    return response;π
-}
-
-fn post_request(path: &str) -> (&str, &str) {
-    ("HTTP/1.1 405 METHOD NOT ALLOWED", "404.html")
+    let parts = if routes.contains_key(path) { routes.get(path).unwrap()() } else { unknown_request() };
+    let response = Response::new(parts.0, parts.1, Some("audio/mpeg"));
+    return response;
 }
 
 fn unknown_request() -> (&'static str, &'static str) {
     ("HTTP/1.1 404 NOT FOUND", "404.html")
 }
 
-fn index(request: &str) -> FileResponse {
+fn index() -> FileResponse {
     ("HTTP/1.1 200 OK", "hello.html")
 }
 
-fn test(request: &str) -> FileResponse {
+fn download() -> FileResponse {
     ("HTTP/1.1 200 OK", "test.mp3")
 }
 
 fn register_routes() -> RouteMap {
     let mut routes: RouteMap = HashMap::new();
     routes.insert("/", index);
-    routes.insert("/test", test);
+    routes.insert("/download", download);
     return routes;
 }
